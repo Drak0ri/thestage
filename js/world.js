1d766f66d69234934de84d8111516f0e4b180e6a
Baz Shaw <baz@iesv.se>
2026-04-10 07:18:11 +0200

    feat: wire WorldObjects into init/switchRoom/resize

--- js/world.js
+++ js/world.js
@@ -20,7 +20,8 @@ const World = {
     this._buildFloor();
     this._buildStars();
     this._buildBgCanvas();
-    window.addEventListener('resize', function() { World._buildBgCanvas(); World.render(); });
+    WorldObjects.init();
+    window.addEventListener('resize', function() { World._buildBgCanvas(); World.render(); WorldObjects.resize(); });
     this.container.addEventListener('click', function(e) {
       var t = e.target;
       if (t === World.container || t.id === 'bg-canvas' ||
@@ -35,6 +36,7 @@ const World = {
     if (!ROOMS[roomId]) return;
     this.currentRoom = roomId;
     Chat.dismissAll();
+    WorldObjects.onRoomSwitch();
     this._buildFloor(); this._buildBgCanvas();
     this.render();
     App.setStatus('Entered ' + ROOMS[roomId].statusLabel);
